ba95a91a750e51a6dc33237ec2724e9327a61857
PBelugin <pashaigood@gmail.com>
2014-09-28 18:12:30 +0400

    Обновлено демо

--- app/index.js
+++ app/index.js
@@ -4,9 +4,10 @@ $(function () {
         console.log(arguments, 'init');
     }
 
-    var recorder = new ig.sound.record.Wav(onInit);
+    var recorder = new ig.sound.record.Wav(onInit),
+        recorder2 = new ig.sound.record.Wav(onInit);
 
-    $('.record').click(function () {
+    function record(recorder) {
         console.log('before start');
         recorder.start(function (err) {
             if (! err) {
@@ -24,5 +25,20 @@ $(function () {
             }
 
         });
+    }
+
+    $('.record').click(function () {
+        record(recorder);
     });
+
+    $('.play').click(function() {
+        recorder.play();
+    });
+
+    $('.record2').click(function () {
+        record(recorder2);
+    });
+    $('.play2').click(function() {
+        recorder2.play();
+    })
 });
