5094e8a4fe6c00cf8e3d73db5590e42456f072ec
PBelugin <pashaigood@gmail.com>
2015-01-20 16:22:04 +0400

    Добавлен 2 вариант записи.

--- app/js/mattdiamond/Worker.js
+++ app/js/mattdiamond/Worker.js
@@ -2,7 +2,8 @@ module.exports = function() {
     var recLength = 0,
         recBuffersL = [],
         recBuffersR = [],
-        sampleRate;
+        sampleRate,
+        bufferSize;
 
     this.onmessage = function(e){
         switch(e.data.command){
@@ -24,15 +25,19 @@ module.exports = function() {
         }
     };
 
-    function init(config){
-        sampleRate = config.sampleRate;
+    function init(cfg){
+        sampleRate = cfg.sampleRate;
+        bufferSize = cfg.bufferSize;
+
     }
 
     function record(inputBuffer){
         recBuffersL.push(inputBuffer[0]);
         recBuffersR.push(inputBuffer[1]);
-        recLength += inputBuffer[0].length;
+        recLength += bufferSize;
     }
+
+
 
     function exportWAV(type){
         var bufferL = mergeBuffers(recBuffersL, recLength);
@@ -82,13 +87,6 @@ module.exports = function() {
         return result;
     }
 
-    function floatTo16BitPCM(output, offset, input){
-        for (var i = 0; i < input.length; i++, offset+=2){
-            var s = Math.max(-1, Math.min(1, input[i]));
-            output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-        }
-    }
-
     function writeString(view, offset, string){
         for (var i = 0; i < string.length; i++){
             view.setUint8(offset + i, string.charCodeAt(i));
@@ -127,8 +125,32 @@ module.exports = function() {
         view.setUint32(40, samples.length * 2, true);
 
         floatTo16BitPCM(view, 44, samples);
-
+//        samplesPCM(samples, view);
         return view;
+    }
+
+    function samplesPCM(interleaved, view)
+    {
+        var lng=interleaved.length;
+        var index=44;
+        var volume=1;
+        var sample=0;
+        for(var i=0;i<lng;i++)
+        {
+            sample = interleaved[i]*(0x7FFF*volume);
+            sample = (sample>0x7FFF)
+                ? 0x7FFF
+                : (sample<-(0x7FFF)) ? -(0x7FFF) : sample;
+            view.setInt16(index,sample,true);
+            index+=2;
+        }
+    }
+
+    function floatTo16BitPCM(output, offset, input){
+        for (var i = 0; i < input.length; i++, offset+=2) {
+            var s = Math.max(-1, Math.min(1, input[i]));
+            output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+        }
     }
 
 };
